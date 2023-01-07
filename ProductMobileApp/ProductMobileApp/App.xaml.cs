3e0db524eee88344ccf254ed83c66b008e46200b
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-01-07 15:41:12 +0600

    Start project

--- ProductMobileApp/ProductMobileApp/App.xaml.cs
+++ ProductMobileApp/ProductMobileApp/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using ProductMobileApp.Services;
-using ProductMobileApp.Views;
-using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
 namespace ProductMobileApp
 {
@@ -13,7 +9,6 @@ namespace ProductMobileApp
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
         }
 
---
+++ ProductMobileApp/ProductMobileApp/Views/ProductPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProductMobileApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductPage : ContentPage
+    {
+        public ProductPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
